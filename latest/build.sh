b1f4d7870e92eb5ead4eb27db1a4bfecb4690a58
OpenSauce04 <opensauce04@gmail.com>
2026-03-12 14:57:12 +0000

    Added build scripts

---
+++ latest/build.sh
@@ -0,0 +1,1 @@
+docker build --no-cache --platform=linux/arm64,linux/amd64 -t opensauce04/azahar-build-environment:latest .
---
+++ transifex/build.sh
@@ -0,0 +1,1 @@
+docker build --no-cache --platform=linux/amd64 -t opensauce04/azahar-build-environment:transifex .
